092748c4f5ce86d37158901197ab00dead8a9a3a
zogoo <chtsogbadrakh@gmail.com>
2025-07-16 01:07:49 +0200

    Skip seed for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+return if Rails.env.production?
+
 include FactoryBot::Syntax::Methods
 
 # Clear existing data (optional)
