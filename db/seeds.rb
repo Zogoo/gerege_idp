6f6edc0ccc410a689ce6dad78d5f36856f87f1d6
zogoo <chtsogbadrakh@gmail.com>
2025-07-16 00:26:48 +0200

    Fix fly deploy issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+include FactoryBot::Syntax::Methods
 
 # Clear existing data (optional)
 puts "Cleaning database..."
